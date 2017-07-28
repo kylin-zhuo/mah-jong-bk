a3508323349324a8c0717a99eae4939c197d1d92
kylinZhuo <kylin.zhuo@gmail.com>
2017-07-28 08:51:52 -0400

    fix

--- mahjong.py
+++ mahjong.py
@@ -6,9 +6,10 @@ num_map = {0:'m', 1:'p', 2:'s', 3:'z'}
 color_list = ['m', 'p', 's', 'z']
 
 # the set at hand
-class Hand(object):
+class Hand:
 
     def __init__(self,
+                 id="",
                  num=0,
                  is_clear=True,
                  mahjongs=[],
@@ -21,6 +22,7 @@ class Hand(object):
         :param exposure: the exposed part of mahjongs
         :param hidden: the still hidden part of mahjongs
         """
+        self.id = id
         self.num = num
         self.is_clear = is_clear
         self.mahjongs = mahjongs
@@ -39,6 +41,9 @@ class Hand(object):
                 )
                 + ch
                 for ch in color_list])
+
+    def take(self, mjs):
+        self.mahjongs += mjs
 
 
 # Mountain. Randomly initialized at the beginning
@@ -61,13 +66,13 @@ class Mountain:
 
         self.mahjongs = regenerate()
         # treasure
-        self.treasure = [self.mountain[-6]]
+        self.treasure = [self.mahjongs[-6]]
         self.pointer = 0
-        self.next = self.mountain[self.pointer]
+        self.next = self.mahjongs[self.pointer]
 
-    def take(self, begin, end):
-        res = self.mountain[self.pointer + begin: self.pointer + end+1]
-        self.pointer += (end - begin + 1)
+    def take(self, num):
+        res = list(self.mahjongs[self.pointer: self.pointer + num])
+        self.pointer += num
         return res
 
 
@@ -82,9 +87,9 @@ class Mahjong:
         self.hold_by = hold_by
 
 
-class Player(object):
+class Player:
 
-    def __init__(self, name, location, is_dealer, points):
+    def __init__(self, name, location, is_dealer, score, hand_id):
         """
         :param name
         :param location: {'e', 's', 'w', 'n'}
@@ -94,28 +99,26 @@ class Player(object):
         self.name = name
         self.location = location
         self.is_dealer = is_dealer
-        self.points = points
-        self.hands = Hand()
+        self.score = score
+        self.hands = Hand(id=hand_id)
 
         self.next = None
 
     def take(self, mjs):
-        self.hands.mahjongs += mjs
-
-
+        self.hands.take(mjs)
 
 
 def initialize_players(score=25000):
 
-    p1 = Player("East", 'e', True, score)
-    p2 = Player("South", 's', False, score)
-    p3 = Player("West", 'w', False, score)
-    p4 = Player("North", 'n', False, score)
+    p1 = Player("East", 'e', True, score, hand_id=1)
+    p2 = Player("South", 's', False, score, hand_id=2)
+    p3 = Player("West", 'w', False, score, hand_id=3)
+    p4 = Player("North", 'n', False, score, hand_id=4)
 
-    p1.next = p2
-    p2.next = p3
-    p3.next = p4
-    p4.next = p1
+    # p1.next = p2
+    # p2.next = p3
+    # p3.next = p4
+    # p4.next = p1
 
     return [p1, p2, p3, p4]
 
@@ -126,24 +129,28 @@ def initialize_mountain():
     return _mountain
 
 
-def distribute(_mountain, _begin_player):
+def distribute(_mountain, players):
+
+    # cp = p1
 
     # take 4 mahjongs each time for 3 times
-    _curr_taker = _begin_player
     for _x in range(3):
         for _y in range(4):
-            mjs = _mountain.take(0,3)
-            _curr_taker.take(mjs)
-            _curr_taker = _curr_taker.next
+            mjs = _mountain.take(4)
+            cp = players[_y]
+            cp.take(mjs)
+            # cp = cp.next
 
     # take the last few
     for _x in range(4):
-        mj = _mountain.take(0, 1)
-        _curr_taker.take(mj)
-        _curr_taker = _curr_taker.next
+        mj = _mountain.take(1)
+        cp = players[_y]
+        cp.take(mj)
+        # cp = cp.next
 
-    mj = mountain.take(0, 1)
-    _curr_taker.take(mj)
+    mj = mountain.take(1)
+    # cp.take(mj)
+    players[0].take(mj)
 
     print "distributed"
 
@@ -152,12 +159,10 @@ if __name__ == '__main__':
 
     # player_1, player_2, player_3, player_4 = initialize_players()
     players = initialize_players()
-    [p1, p2, p3, p4] = players
+
     mountain = initialize_mountain()
-    distribute(mountain, p1)
+    distribute(mountain, players)
 
-
-
-
-
-
+    [p1, p2, p3, p4] = players
+    print p1.hands.mahjongs
+    # print mountain.mahjongs
